786ed83fc92ac44ece8ace82c88b62da242efd7f
Takhmina Iliiasova <mina@ip-10-36-29-70.dynamic.yorku.ca>
2026-02-04 04:54:46 -0500

    Updated values

--- r_carr_cap_plot.py
+++ r_carr_cap_plot.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 
 # Data
 r_values = [1, 2, 3, 5, 10, 15, 19]
-carrying_capacities = [33.36, 83.01, 106.58, 127.54, 137.63, 144.92, 146.05]
-
+carrying_capacities = [3.64, 74.44,103.35, 126.71, 137.21, 144.92, 146.05]
 # Plot
 plt.figure(figsize=(8, 6))
 plt.plot(r_values, carrying_capacities, '-o')  # Line with markers
-plt.xlabel('Moore Radius r')
+plt.xlabel('Moore radius ρ')
 plt.ylabel('Carrying Capacity (K)')
-plt.title('Carrying Capacity vs Moore Radius r (Alpha = 0)')
+#plt.title('Carrying Capacity vs Moore Radius r (Alpha = 0)')
+
+plt.xticks(r_values)
 plt.grid(True)
 plt.show()
